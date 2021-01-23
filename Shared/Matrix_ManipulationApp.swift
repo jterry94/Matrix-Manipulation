6beb4772fa794468fa02e73ec3260400a71212aa
Jeff Terry <terryj@iit.edu>
2021-01-23 13:40:15 -0600

    Initial Commit

---
+++ Shared/Matrix_ManipulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matrix_ManipulationApp.swift
+//  Shared
+//
+//  Created by Jeff Terry on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Matrix_ManipulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
